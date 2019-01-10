ec59493dd6f5289e9aaee58ece6041215a5dad33
Casey Wang <caseywang@httpeace.com>
2019-01-10 23:53:54 +0800

    create rotate animation

--- src/Navigator/Screens/Home/Animation.js
+++ src/Navigator/Screens/Home/Animation.js
@@ -1,24 +1,30 @@
 import React from "react";
-import { View, Text } from "react-native";
-import { Button, Card, Title, Paragraph } from "react-native-paper";
+import { View } from "react-native";
+import { Button, Card, Title, Paragraph, IconButton } from "react-native-paper";
 import { Animated, Easing, StyleSheet } from "react-native";
 export default class Animation extends React.Component {
   constructor(props) {
     super(props);
     // TIPS:
     // 不要把这些值setState
-    this.scaleAnimation = new Animated.Value(0);
-    this.cardScale = this.scaleAnimation.interpolate({
+    this.scaleValue = new Animated.Value(0);
+    this.cardScale = this.scaleValue.interpolate({
       inputRange: [0, 0.5, 1],
       outputRange: [1, 1.1, 1]
+    });
+    this.rotateValue = new Animated.Value(0);
+    this.fabRotate = this.rotateValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ["0deg", "360deg"]
     });
   }
   render() {
     return (
-      <View styles={[styles.container]}>
+      <View style={[styles.container]}>
         {/* Scale Animation */}
+        {/* TIPS: wrapper Animated 组件 */}
         <Animated.View style={{ transform: [{ scale: this.cardScale }] }}>
-          <Card style={{ margin: 16 }}>
+          <Card>
             <Card.Content>
               <Title>Card title</Title>
               <Paragraph>Card content</Paragraph>
@@ -27,8 +33,8 @@ export default class Animation extends React.Component {
             <Card.Actions>
               <Button
                 onPress={() => {
-                  this.scaleAnimation.setValue(0);
-                  Animated.timing(this.scaleAnimation, {
+                  this.scaleValue.setValue(0);
+                  Animated.timing(this.scaleValue, {
                     toValue: 1,
                     duration: 250,
                     easing: Easing.linear,
@@ -41,10 +47,34 @@ export default class Animation extends React.Component {
             </Card.Actions>
           </Card>
         </Animated.View>
+        {/* Rotate Animation */}
+        <Animated.View
+          style={{
+            alignContent: "center",
+            alignItems: "center",
+            margin: 16,
+            transform: [{ rotate: this.fabRotate }]
+          }}
+        >
+          <IconButton
+            icon="cached"
+            onPress={() => {
+              this.rotateValue.setValue(0);
+              Animated.timing(this.rotateValue, {
+                toValue: 1,
+                duration: 700,
+                easing: Easing.linear
+              }).start();
+            }}
+          />
+        </Animated.View>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    flex: 1,
+    padding: 16
+  }
 });
--- src/Navigator/Screens/Home/Home.js
+++ src/Navigator/Screens/Home/Home.js
@@ -3,7 +3,13 @@ import { View, Text } from "react-native";
 export default class Home extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
         <Text>Home Screen</Text>
       </View>
     );
