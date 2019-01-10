a3c6ef08f973d58603df98c390aae4f7d6aa66d8
Casey Wang <caseywang@httpeace.com>
2019-01-10 21:44:10 +0800

    change launch root file

---
+++ src/App/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
+export default class App extends React.Component {
+    render() {
+        return (
+            <PaperProvider>
+                <View style={styles.container}>
+                    <Text>Open up App.js to start working on your app!</Text>
+                </View>
+            </PaperProvider>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    }
+});
